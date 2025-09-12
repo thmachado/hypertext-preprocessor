cedacd65e25f70b95902fcc4b5b7792a3c1b1744
thmachado <thiago.wimps@gmail.com>
2025-09-12 19:20:14 -0300

    feat: abstract classes -> simulate payment

---
+++ tests/Payment/BankTest.php
@@ -0,0 +1,100 @@
+<?php
+
+declare(strict_types=1);
+
+use App\Models\User;
+use App\Payment\Nubank;
+use App\Payment\Picpay;
+use PHPUnit\Framework\TestCase;
+
+final class BankTest extends TestCase
+{
+    private Nubank $nubank;
+    private Picpay $picpay;
+
+    protected function setUp(): void
+    {
+        $this->nubank = new Nubank(new User("Thiago", "Machado", "thiago@nubank.com"), 10000);
+        $this->picpay = new Picpay(new User("Raphael", "Veiga", "raphaelveiga@email.com"), 15000);
+    }
+
+    public function testDepositLessThanZero(): void
+    {
+        $this->expectException(RuntimeException::class);
+        $this->expectExceptionMessage("Must be greater than zero");
+        $this->picpay->deposit(0);
+    }
+
+    public function testDeposit(): void
+    {
+        $nubank = $this->nubank->deposit(500);
+        $picpay = $this->picpay->deposit(100);
+
+        $this->assertIsFloat($nubank);
+        $this->assertIsFloat($picpay);
+        $this->assertEquals(10500, $nubank);
+        $this->assertEquals(15100, $picpay);
+    }
+
+    public function testWithdrawLessThanZero(): void
+    {
+        $this->expectException(RuntimeException::class);
+        $this->expectExceptionMessage("Must be greater than zero");
+        $this->nubank->withdraw(0);
+    }
+
+    public function testWithdrawInsufficientBalance(): void
+    {
+        $this->expectException(RuntimeException::class);
+        $this->expectExceptionMessage("Insufficient balance");
+        $this->picpay->withdraw(10000000);
+    }
+
+    public function testWithdraw(): void
+    {
+        $nubank = $this->nubank->withdraw(500);
+        $picpay = $this->picpay->withdraw(100);
+
+        $this->assertIsFloat($nubank);
+        $this->assertIsFloat($picpay);
+        $this->assertEquals(9500, $nubank);
+        $this->assertEquals(14900, $picpay);
+    }
+
+    public function testTransferLessThanZero(): void
+    {
+        $this->expectException(RuntimeException::class);
+        $this->expectExceptionMessage("Must be greater than zero");
+        $this->nubank->transfer($this->picpay, 0);
+    }
+
+    public function testTransferInsufficientBalance(): void
+    {
+        $this->expectException(RuntimeException::class);
+        $this->expectExceptionMessage("Insufficient balance");
+        $this->picpay->transfer($this->nubank, 10000000);
+    }
+
+    public function testTransfer(): void
+    {
+        $picpay = $this->picpay->transfer($this->nubank, 500);
+        $nubank = $this->nubank->transfer($this->picpay, 500);
+
+        $this->assertIsFloat($picpay);
+        $this->assertIsFloat($nubank);
+        $this->assertEquals(14500, $picpay);
+        $this->assertEquals(10000, $nubank);
+
+    }
+
+    public function testBalance(): void
+    {
+        $nubank = $this->nubank->balance();
+        $picpay = $this->picpay->balance();
+
+        $this->assertIsFloat($nubank);
+        $this->assertIsFloat($picpay);
+        $this->assertEquals(15000, $picpay);
+        $this->assertEquals(10000, $nubank);
+    }
+}
